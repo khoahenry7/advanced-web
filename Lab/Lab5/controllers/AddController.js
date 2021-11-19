4387abcc727c00f7313b905affce933b7e9dc1fc
KhoaHenry <trananhkhoa73@gmail.com>
2021-11-19 13:06:30 +0700

    Add new user

--- Lab/Lab5/controllers/AddController.js
+++ Lab/Lab5/controllers/AddController.js
@@ -6,31 +6,35 @@ class AddController {
     }
 
     post(req, res) {
-        let user = JSON.stringify(req.body)
+        let user = JSON.stringify({
+            name: req.body.name,
+            age: parseInt(req.body.age),
+            gender: req.body.gender,
+            email: req.body.email
+        })
 
         const options = {
             hostname: 'web-nang-cao.herokuapp.com',
             port: 443,
             path: '/lab5/users',
             method: 'POST',
-            // headers: { 'Content-Type': "application/json"},
-            // data: user,
-            // body: user,
-            // "name": user.name,
-            // user: user
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': user.length,
+            },
         }
 
         const request = https.request(options, response => {
             let users
             response.on('data', data => {
                 users = JSON.parse(data)
-                console.log(users)
             })
             response.on('end', () => {
-                // res.redirect("../")
+                res.redirect("../")
             })
         })
-        // res.redirect('/add')
+
+        request.write(user) // post
         request.end()
     }
 }
